1dad7e97153e86dd176ccf821a8440a3baa4f801
enviu <michalkml13@gmail.com>
2020-11-04 16:27:57 +0100

    portfolio done

---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import Footer from '../components/Footer'
+import HeroSection from '../components/HeroSection'
+import InfoSection from '../components/InfoSection'
+import { HomeObjOne, HomeObjTwo, HomeObjThree } from '../components/InfoSection/Data'
+import Navbar from '../components/Navbar'
+import Services from '../components/Services'
+import Sidebar from '../components/Sidebar'
+
+const Home = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar toggle={toggle} />
+            <HeroSection />
+            <InfoSection {...HomeObjOne} />
+            <InfoSection {...HomeObjTwo} />
+            <Services />
+            <InfoSection {...HomeObjThree} />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
